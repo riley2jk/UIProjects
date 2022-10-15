e3cea58a22795b4f59fd321fa7d7f2290ee5306f
riley2jk <65510000+riley2jk@users.noreply.github.com>
2022-10-14 23:24:41 -0400

    final Proj 1

--- Project1/main.js
+++ Project1/main.js
@@ -1,10 +1,11 @@
-let data = {level: 10, target: 61, active: 2, feet: 5, inch: 10, goal: 24, temp: 60, consum: 13, YVals: [17,22,25,18,15,29,23,14], XVals: ["Mon","Tues","Wed","Thur","Fri","Sat","Sun","Cur"]};
+let data = {level: 10, target: 61, active: 2, feet: 5, inch: 10, weight: 185, goal: 24, temp: 60, consum: 13, YVals: [17,22,25,18,15,29,23,14], XVals: ["Mon","Tues","Wed","Thur","Fri","Sat","Sun","Cur"], charge: 3};
 
 function setValues() {
     document.getElementById("modal-target-temp").value = data.target;
     document.getElementById("modal-target-active").value = data.active;
     document.getElementById("modal-target-feet").value = data.feet;
     document.getElementById("modal-target-inch").value = data.inch;
+    document.getElementById("modal-target-weight").value = data.weight;
     document.getElementById("goal-num").innerHTML = data.goal.toString();
     document.getElementById("consum-num").innerHTML = data.consum.toString();
     document.getElementById("target-num").innerHTML = "Target: " + data.target + "°F";
@@ -17,18 +18,20 @@ var abtn = document.getElementById("aopen");
 var tspan = document.getElementsByClassName("close")[0];
 var aspan = document.getElementsByClassName("close")[1];
 setInterval(tempChange, 5000);
+Chart.defaults.global.defaultFontColor = "#fff";
 
-function getPlot(newDay) {
+function getPlot(e) {
+    var newDay = Math.floor(e);
     var yValues = getYVals(newDay, data.YVals);
         
     new Chart("myPlot", {
         type: "line",
         data: {
-            labels: newDay == 0 ? getXVals(data.XVals) : data.XVals,
+            labels: newDay == 0 ? data.XVals : getXVals(data.XVals),
             datasets: [{
                 fill: true,
                 lineTension: 0,
-                backgroundColor: "#00cdff",
+                backgroundColor: "#54bbe0",
                 borderColor: "#fff",
                 data: yValues,
                 }]
@@ -36,13 +39,19 @@ function getPlot(newDay) {
         options: {
             legend: {display: false},
             scales: {
-                yAxes: [{ticks: {min: 0, max:35}}]
+                yAxes: [{
+                    ticks: {min: 0, max:35},
+                    gridLines: {color: "#222"}
+                }],
+                xAxes: [{
+                    gridLines: {color: "#222"}
+                }]
             },
         },
-        defaults: {
-            color: "rgba(255,255,255,1)"
-        }
     });
+
+    console.log(data.XVals);
+    console.log(data.YVals);
 };
 
 function getYVals(newDay, YVals) {
@@ -103,7 +112,7 @@ $("#tempSlider").roundSlider({
             color = "#ff2a00"
         }
         else if (e.value < data.target) {
-            color = "#00cdff"
+            color = "#54bbe0"
         }
         else {
             color = "#fff"
@@ -170,10 +179,11 @@ aspan.onclick = function() {
 function setModalStatus() {
     data.inch = Math.floor(document.getElementById("modal-target-inch").value);
     data.feet = Math.floor(document.getElementById("modal-target-feet").value);
+    data.weight = Math.floor(document.getElementById("modal-target-weight").value);
     data.active = Math.floor(document.getElementById("modal-target-active").value);
     data.target = Math.floor(document.getElementById("modal-target-temp").value);
 
-    data.goal = (2 * data.feet) + data.active + Math.floor((data.inch / 5)) + 10;
+    data.goal = (2 * data.feet) + data.active + Math.floor(data.inch / 5) + Math.floor(data.weight / 20);
 
     setValues();
 
@@ -218,6 +228,12 @@ document.addEventListener("keydown", (e) => {
             getPlot(1);
             setValues();
             break;
+        case "b":
+            battChange(0);
+            break;
+        case "c":
+            battChange(1);
+            break;
         default:
             break;
     }
@@ -234,4 +250,30 @@ function tempChange() {
     }
     
     resetTempSlider();
+}
+
+function battChange(e) {
+    if (e == 0 && data.charge != 0) {
+        data.charge -= 1;
+    }
+    else if (e == 1 && data.charge != 3) {
+        data.charge += 1;
+    }
+
+    var main = document.getElementsByClassName("main")[0];
+
+    switch(data.charge) {
+        case 0:
+            main.style.borderColor = "#be2802";
+            break;
+        case 1:
+            main.style.borderColor = "#bdba00";
+            break;
+        case 2:
+            main.style.borderColor = "#7daa02";
+            break;
+        case 3:
+            main.style.borderColor = "#00b33c";
+            break;
+    }
 }
